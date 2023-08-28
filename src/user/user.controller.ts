217309fe9b360597597506485b47e0e051c9791a
GabriielB <gabrielbraga1953@hotmail.com>
2023-08-28 11:57:28 -0300

    add: user service & user interface

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import {createUserDto} from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
 
+    constructor (private readonly userService: UserService) {
+
+    }
+
     @Get()
-    async getAllUsers(){
-        return JSON.stringify({test : 'abc'})
+    async getAllUsers(): Promise<User[]>{
+        return this.userService.getAllUser();
     };
 
     @Post()
-    async createUser(@Body() createUser: createUserDto){
-        return createUser
+    async createUser(@Body() createUser: createUserDto): Promise<User>{
+        return this.userService.createUser(createUser);
     }
 }
